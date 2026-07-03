b3cd10bb16d30da45143ef8a299fefcf2d977db5
zeron953 <ardakala474@gmail.com>
2026-07-03 23:15:45 +0300

    asdasd
    asdasd

---
+++ output/buttons.hpp
@@ -0,0 +1,29 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-07-03 20:12:57.451114100 UTC
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    // Module: client.dll
+    namespace buttons {
+        constexpr std::ptrdiff_t attack = 0x2065A90;
+        constexpr std::ptrdiff_t attack2 = 0x2065B20;
+        constexpr std::ptrdiff_t back = 0x2065D60;
+        constexpr std::ptrdiff_t duck = 0x2066030;
+        constexpr std::ptrdiff_t forward = 0x2065CD0;
+        constexpr std::ptrdiff_t jump = 0x2065FA0;
+        constexpr std::ptrdiff_t left = 0x2065DF0;
+        constexpr std::ptrdiff_t lookatweapon = 0x2355FE0;
+        constexpr std::ptrdiff_t reload = 0x2065A00;
+        constexpr std::ptrdiff_t right = 0x2065E80;
+        constexpr std::ptrdiff_t showscores = 0x2355EC0;
+        constexpr std::ptrdiff_t sprint = 0x2065970;
+        constexpr std::ptrdiff_t turnleft = 0x2065BB0;
+        constexpr std::ptrdiff_t turnright = 0x2065C40;
+        constexpr std::ptrdiff_t use = 0x2065F10;
+        constexpr std::ptrdiff_t zoom = 0x2355F50;
+    }
+}
---
+++ output/buttons.zig
@@ -0,0 +1,24 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-07-03 20:12:57.451114100 UTC
+
+pub const cs2_dumper = struct {
+    // Module: client.dll
+    pub const buttons = struct {
+        pub const attack: usize = 0x2065A90;
+        pub const attack2: usize = 0x2065B20;
+        pub const back: usize = 0x2065D60;
+        pub const duck: usize = 0x2066030;
+        pub const forward: usize = 0x2065CD0;
+        pub const jump: usize = 0x2065FA0;
+        pub const left: usize = 0x2065DF0;
+        pub const lookatweapon: usize = 0x2355FE0;
+        pub const reload: usize = 0x2065A00;
+        pub const right: usize = 0x2065E80;
+        pub const showscores: usize = 0x2355EC0;
+        pub const sprint: usize = 0x2065970;
+        pub const turnleft: usize = 0x2065BB0;
+        pub const turnright: usize = 0x2065C40;
+        pub const use: usize = 0x2065F10;
+        pub const zoom: usize = 0x2355F50;
+    };
+};
