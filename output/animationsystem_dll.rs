59ce7ccf4352fcfbc0b8b4c403d7c1143e1c5f3e
zeron953 <ardakala474@gmail.com>
2026-07-10 01:06:08 +0300

    S
    S

--- output/animationsystem_dll.rs
+++ output/animationsystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-09 10:05:33.799860200 UTC
+// 2026-07-09 22:05:49.291244900 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- output/client_dll.cs
+++ output/client_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-09 10:05:33.799860200 UTC
+// 2026-07-09 22:05:49.291244900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
